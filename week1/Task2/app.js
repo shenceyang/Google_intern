99c8769774df594bad81cdd74dbcb0f97795ed3b
Shence <ysc1412799032@163.com>
2024-01-12 18:50:17 -0500

    T2,3,4 completed

---
+++ week1/Task2/app.js
@@ -0,0 +1,4 @@
+const action = require('./helper_function.js');
+
+action.attact('Shence');
+action.running('Ben');
---
+++ week1/Task2/helper_function.js
@@ -0,0 +1,12 @@
+function attact(Name){
+    console.log(Name +" is attacting" );
+}
+
+function running(Name){
+    console.log(Name +" is running" );
+}
+
+module.exports = {
+    attact: attact,
+    running: running
+}
