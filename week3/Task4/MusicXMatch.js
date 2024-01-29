d3b8cf4a980cfc1467d10f499f0864b05c06e5e0
Shence <ysc1412799032@163.com>
2024-01-28 20:04:15 -0500

    w3:t4

---
+++ week3/Task4/MusicXMatch.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const apiKey = '76e6e2c63b078f26fc41972be4ccada5';
+const artistName = 'Justin Bieber';
+const trackTitle = 'Baby';
+
+const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${trackTitle}&q_artist=${artistName}&apikey=${apiKey}`;
+
+axios.get(url)
+  .then(response => {
+    // Process the response data here
+    console.log(response.data.message.body.lyrics.lyrics_body);
+  })
+  .catch(error => {
+    console.error('Error fetching data: ', error);
+  });
